37616153dc238ae5e8754407ebf73ac1f0e2c4c1
Copons <copons@users.noreply.github.com>
2017-06-30 13:08:15 +0100

    Fix expandable textarea overflow and resize

--- client/blocks/comment-detail/comment-detail-reply.jsx
+++ client/blocks/comment-detail/comment-detail-reply.jsx
@@ -13,15 +13,42 @@ import { get } from 'lodash';
 import AutoDirection from 'components/auto-direction';
 import { getCurrentUser } from 'state/current-user/selectors';
 
+const TEXTAREA_MAX_HEIGHT = 236; // 10 lines
+const TEXTAREA_MIN_HEIGHT_COLLAPSED = 47;
+const TEXTAREA_MIN_HEIGHT_FOCUSED = 68; // 2 lines
+const TEXTAREA_VERTICAL_BORDER = 2;
+
 export class CommentDetailReply extends Component {
 	state = {
 		commentText: '',
 		hasFocus: false,
+		textareaMinHeight: TEXTAREA_MIN_HEIGHT_COLLAPSED,
 	};
 
-	handleTextChange = event => this.setState( { commentText: event.target.value } );
+	bindTextareaRef = textarea => {
+		this.textarea = textarea;
+	}
 
-	setFocus = () => this.setState( { hasFocus: true } );
+	calculateTextareaMinHeight = () => {
+		const textareaScrollHeight = this.textarea.scrollHeight;
+		const textareaMinHeight = Math.min( TEXTAREA_MAX_HEIGHT, textareaScrollHeight + TEXTAREA_VERTICAL_BORDER );
+		return Math.max( TEXTAREA_MIN_HEIGHT_FOCUSED, textareaMinHeight );
+	}
+
+	handleTextChange = event => {
+		const { value } = event.target;
+		const textareaMinHeight = this.calculateTextareaMinHeight();
+
+		this.setState( {
+			commentText: value,
+			textareaMinHeight,
+		} );
+	}
+
+	setFocus = () => this.setState( {
+		hasFocus: true,
+		textareaMinHeight: this.calculateTextareaMinHeight(),
+	} );
 
 	submit = () => {
 		const {
@@ -61,11 +88,18 @@ export class CommentDetailReply extends Component {
 		}
 	}
 
-	unsetFocus = () => this.setState( { hasFocus: false } );
+	unsetFocus = () => this.setState( {
+		hasFocus: false,
+		textareaMinHeight: TEXTAREA_MIN_HEIGHT_COLLAPSED,
+	} );
 
 	render() {
 		const { translate } = this.props;
-		const { commentText, hasFocus } = this.state;
+		const {
+			commentText,
+			hasFocus,
+			textareaMinHeight,
+		} = this.state;
 
 		const hasCommentText = commentText.trim().length > 0;
 
@@ -74,32 +108,44 @@ export class CommentDetailReply extends Component {
 			'is-visible': hasFocus || hasCommentText,
 		} );
 
-		const expandingAreaClasses = classNames( 'is-expanding-area', {
+		const textareaClasses = classNames( {
 			'is-focused': hasFocus,
 		} );
 
+		const textareaStyle = {
+			minHeight: textareaMinHeight,
+		};
+		if ( ! hasFocus ) {
+			// Force the textarea to collapse even if it was manually resized
+			textareaStyle.height = TEXTAREA_MIN_HEIGHT_COLLAPSED;
+		}
+		if ( textareaMinHeight === TEXTAREA_MAX_HEIGHT ) {
+			// Only show the scrollbar if the textarea content exceeds the max height
+			textareaStyle.overflow = 'auto';
+		}
+
 		return (
 			<form className="comment-detail__reply">
-				<div className={ expandingAreaClasses }>
-					<pre><span>{ commentText }</span><br /></pre>
-					<AutoDirection>
-						<textarea
-							onBlur={ this.unsetFocus }
-							onChange={ this.handleTextChange }
-							onFocus={ this.setFocus }
-							onKeyDown={ this.submitCommentOnCtrlEnter }
-							placeholder={ translate( 'Enter your comment here…' ) }
-							value={ commentText }
-						/>
-					</AutoDirection>
-					<button
-						className={ buttonClasses }
-						disabled={ ! hasCommentText }
-						onClick={ this.submitComment }
-					>
-						{ translate( 'Send' ) }
-					</button>
-				</div>
+				<AutoDirection>
+					<textarea
+						className={ textareaClasses }
+						onBlur={ this.unsetFocus }
+						onChange={ this.handleTextChange }
+						onFocus={ this.setFocus }
+						onKeyDown={ this.submitCommentOnCtrlEnter }
+						placeholder={ translate( 'Enter your comment here…' ) }
+						ref={ this.bindTextareaRef }
+						style={ textareaStyle }
+						value={ commentText }
+					/>
+				</AutoDirection>
+				<button
+					className={ buttonClasses }
+					disabled={ ! hasCommentText }
+					onClick={ this.submitComment }
+				>
+					{ translate( 'Send' ) }
+				</button>
 			</form>
 		);
 	}
